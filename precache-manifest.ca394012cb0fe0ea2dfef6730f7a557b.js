eacb3b33ece8f18b6afd1008bf94ec2ac74cacbb
yanyuanfeng@minshenglife.com <yanyuanfeng@minshenglife.com>
2018-12-18 17:43:04 +0800

    ..

---
+++ precache-manifest.ca394012cb0fe0ea2dfef6730f7a557b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aee513e65a00b7342a9",
+    "url": "static/js/chunk-vendors.d67bd7d2.js"
+  },
+  {
+    "revision": "e940dc4918c19425877b",
+    "url": "static/js/app.287d2971.js"
+  },
+  {
+    "revision": "4c8d72d2c1d0b23b2fac",
+    "url": "static/js/about.cd32f879.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e940dc4918c19425877b",
+    "url": "static/css/app.91a8f85d.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8358f54c3640fbcd301b171a546c166a",
+    "url": "index.html"
+  }
+];
